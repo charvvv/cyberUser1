9cb0e4949c4bc6edf2e3557b1a7b9b037b472a22
charvi <63304141+charvvv@users.noreply.github.com>
2025-07-07 08:05:08 -0500

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import BlogPosts from './BlogPosts'; 
+import SignUpScreen from './SignUpScreen';
+import SignInScreen from './SignInScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="BlogPosts">
+        <Stack.Screen name="BlogPosts" component={BlogPosts} options={{ title: 'Cyber Blog' }} />
+        <Stack.Screen name="SignUp" component={SignUpScreen} options={{ title: 'Sign Up' }} />
+        <Stack.Screen name="SignIn" component={SignInScreen} options={{ title: 'Sign In' }} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
